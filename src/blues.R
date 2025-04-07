763fd52722df58e4a444f15473589da3e6a58158
will-the-g <wgatlin@chapman.edu>
2025-04-07 16:05:03 -0700

    changed blues.R to not rely on asrmel package

--- src/blues.R
+++ src/blues.R
@@ -1,23 +1,11 @@
-library(asreml)
-library(ggplot2)
+# Load required packages
+library(lme4)
+library(dplyr)
 
-asreml.options(
-  workspace = '4gb',
-  pworkspace = '4gb',
-  maxit = 300
-)
+# Read dataset
+data <- read.csv("data/Training Data/1_Training_Trait_Data_2014_2021.csv")
 
-plot_field <- function(env) {
-  desplot::desplot(
-    data[data$Env == env, ],
-    Block ~ Range + Pass,
-    out1 = Experiment,
-    text = Replicate,
-    cex = 1,
-    ticks = T,
-    main = env
-  )
-}
+# Filter for the environment of interest
 
 envs2019 <- c('DEH1_2019', 'TXH2_2019', 'NCH1_2019', 'SCH1_2019', 'IAH3_2019', 'MNH1_2019', 'IAH2_2019', 'TXH3_2019', 'NYH3_2019', 'ILH1_2019',
               'WIH1_2019', 'GAH1_2019', 'WIH2_2019', 'TXH1_2019', 'IAH4_2019', 'MIH1_2019', 'INH1_2019', 'GEH1_2019', 'IAH1_2019', 'NYH2_2019', 
@@ -32,159 +20,56 @@ envs2021 <- c('COH1_2021', 'DEH1_2021', 'GAH1_2021', 'GAH2_2021', 'GEH1_2021', '
 
 envs <- c(envs2019, envs2020, envs2021)
 
-data <- read.csv('data/Training_Data/1_Training_Trait_Data_2014_2021.csv')
-data <- data[data$Env %in% envs, c(1:10, 24)]
-data$Field_Location <- NULL
-data <- data[data$Hybrid != 'LOCAL_CHECK', ]
-data <- data[order(data$Experiment), ]  # to use heterogeneous variances if needed
-rownames(data) <- NULL 
-data$rep <- interaction(data$Replicate, data$Block)
-for (variable in c('Env', 'Experiment', 'Replicate', 'Block', 'rep', 'Plot', 'Range', 'Pass', 'Hybrid')) {
-  data[, variable] <- factor(data[, variable])
-}
-
-# droplevels(data[(grepl('^W10', data$Hybrid) == FALSE) & (data$Year == 2020), ])$Hybrid 
-# with(droplevels(data[data$Env == 'WIH1_2021', ]), table(Hybrid, Block))
-plot_field('NCH1_2020')
-plot_field('IAH3_2021')
-
-# NYS1_2020 has one block only
-# plot_field('NYS1_2020')
-
-# trial
-data_NCH1_2020 <- data[data$Env == 'NCH1_2020', ]
-data_NCH1_2020 <- droplevels(data_NCH1_2020)
-with(data_NCH1_2020, table(Range))
-with(data_NCH1_2020, table(Pass))
-with(data_NCH1_2020, table(Block))
-with(data_NCH1_2020, table(Replicate))
-with(data_NCH1_2020, table(Replicate, Block))
-with(data_NCH1_2020, table(Hybrid))
-with(data_NCH1_2020, table(Hybrid)) |> table()
-with(data_NCH1_2020, table(Hybrid)) |> table() |> prop.table() |> round(3)
-data_NCH1_2020[data_NCH1_2020$Hybrid == '2369/LH123HT', ]
-
-# another location
-with(data[data$Env == 'IAH3_2021', ], table(Hybrid)) |> table()
-with(data[data$Env == 'IAH3_2021', ], table(Hybrid)) |> table() |> prop.table() |> round(4)
-
-# hybrids per env
-colSums(aggregate(Env ~ Hybrid, FUN = table, data = data)[, -1])
-
-# number of hybrid replications per env
-rep_hybrids <- data.frame()
 for (env in envs) {
-  tab <- with(data[data$Env == env, ], table(Hybrid)) |> table()
-  tab <- as.data.frame(tab)
-  tab$Env <- env
-  rep_hybrids <- rbind(rep_hybrids, tab)
-}
-colnames(rep_hybrids) <- c('Reps', 'Freq', 'Env')
-rep_hybrids <- rep_hybrids[rep_hybrids$Reps != 0, ]
-rownames(rep_hybrids) <- NULL
-rep_hybrids <- transform(rep_hybrids, Reps = factor(Reps), Freq = Freq, Env = factor(Env, levels = envs))
-ggplot(rep_hybrids, aes(x = Reps, y = Freq)) +
-  geom_col() +
-  facet_wrap(~Env, scales = "free_x")
-
-# testers per environment
-data$Tester <- as.factor(gsub('.*\\/', '', data$Hybrid))
-tab_env_tester <- with(data[!is.na(data$Yield_Mg_ha), ], table(Env, Tester))[envs, ]
-heatmap(tab_env_tester)
-
-#############################################
-# single-environment models
-# Y = mu + Hybrid + Rep + (1 | Rep:Block + Column + Row) + e
-
-blues <- data.frame()
-cvs_h2s <- data.frame()
-for (env in envs) {
-  cat(env, '\n')
   
-  # for blues
-  fixed <- as.formula('Yield_Mg_ha ~ Hybrid + Replicate')
-  random <- c('Replicate:Block', 'Range', 'Pass')
-
-  # for heritability
-  fixed_h2 <- as.formula('Yield_Mg_ha ~ Replicate')
-  random_h2 <- c('Hybrid', 'Replicate:Block', 'Range', 'Pass')
+  cat("Processing environment:", env, "\n")
   
-  data_env <- droplevels(data[data$Env == env, ])
-  if (all(is.na(data_env$Range)) == T) {
-    cat('Removing Range factor', '\n')
-    random <- random[random != 'Range']
-    random_h2 <- random_h2[random_h2 != 'Range']
-  }
-  if (all(is.na(data_env$Pass)) == T) {
-    cat('Removing Pass factor', '\n')
-    random <- random[random != 'Pass']
-    random_h2 <- random_h2[random_h2 != 'Pass']
-  }
-  if (length(unique(data_env$Block)) == 1) {
-    cat('Removing nesting Block factor', '\n')
-    random[random == 'Replicate:Block'] <- 'Replicate'
-    random_h2[random_h2 == 'Replicate:Block'] <- 'Replicate'
-  }
-  if (env == 'WIH1_2021') {
-    cat('Removing Range due singularity with block', '\n')
-    random <- random[random != 'Range']
-    random_h2 <- random_h2[random_h2 != 'Range']
+  # Filter for the environment
+  env_data <- data %>% filter(Env == env)
+  
+  # Convert categorical variables to factors
+  env_data$Hybrid <- as.factor(env_data$Hybrid)
+  env_data$Replicate <- as.factor(env_data$Replicate)
+  
+  # Check for sufficient data to fit the model
+  if (nlevels(env_data$Hybrid) < 2 || nlevels(env_data$Replicate) < 1) {
+    cat("Skipping environment due to insufficient levels for Hybrid or Replicate.\n")
+    next
   }
   
-  # blues
-  random <- as.formula(paste0('~', paste0(random, collapse = '+')))
-  mod <- asreml(
-    fixed = fixed, random = random, data = data, subset = data$Env == env, 
-    na.action = na.method(x = 'omit', y = 'include')
-  )
-  pred <- predict.asreml(mod, classify = 'Hybrid')$pvals[, 1:2]
-  pred$Env <- env
-  cat('BLUEs:\n')
-  print(summary(pred$predicted.value))
-  cat('\n')
-  blues <- rbind(blues, pred)
+  # Fit a linear mixed-effects model
+  model <- lmer(Yield_Mg_ha ~ Hybrid + (1 | Replicate), data = env_data)
   
-  # CV
-  res_var <- summary(mod)$varcomp[which(rownames(summary(mod)$varcomp) == 'units!R'), 'component']
-  cv <- sqrt(res_var) / mean(pred$predicted.value, na.rm = TRUE)
-  cat('CV:', cv)
-  cat('\n')
-  cv_h2 <- data.frame(Env = env, cv = cv)
+  # Extract fixed effects (BLUEs)
+  blues <- as.data.frame(fixef(model))
+  blues <- data.frame(Hybrid = rownames(blues), BLUE = blues[,1])
+  blues$BLUE <- blues$BLUE + fixef(model)["(Intercept)"]
   
-  # heritability
-  random_h2 <- as.formula(paste0('~', paste0(random_h2, collapse = '+')))
-  mod <- asreml(
-    fixed = fixed_h2, random = random_h2, data = data, subset = data$Env == env,
-    predict = predict.asreml(classify = 'Hybrid', sed = TRUE),
-    na.action = na.method(x = 'omit', y = 'include')
-  )
-  h2 <- 1 - ((mod$predictions$avsed['mean'] ^ 2) / (2 * summary(mod)$varcomp['Hybrid', 'component']))
-  h2 <- unname(h2)
-  cat('H2:', h2)
-  cat('\n')
-  cv_h2$h2 <- h2
-  cvs_h2s <- rbind(cvs_h2s, cv_h2)
+  # Save results
+  output_path <- paste0("output/blues_mixed_model_", env, ".csv")
+  write.csv(blues, output_path, row.names = FALSE)
   
-  cat('-----------------------------\n\n')
+  # Print first few BLUEs
+  print(head(blues))
+  
+  # Calculate Coefficient of Variation (CV)
+  residual_variance <- summary(model)$sigma^2
+  mean_yield <- mean(env_data$Yield_Mg_ha, na.rm = TRUE)
+  cv <- sqrt(residual_variance) / mean_yield
+  cat("CV for", env, ":", cv, "\n")
+  
+  # Calculate Heritability (H^2)
+  var_components <- as.data.frame(VarCorr(model))
+  
+  # Check if Hybrid variance exists
+  if ("Hybrid" %in% var_components$grp) {
+    var_hybrid <- var_components$vcov[var_components$grp == "Hybrid"]
+  } else {
+    var_hybrid <- 0
+  }
+  
+  heritability <- var_hybrid / (var_hybrid + residual_variance)
+  cat("Heritability (H²) for", env, ":", heritability, "\n\n")
 }
 
-
-cat('Corr(CV, h2):', cor(cvs_h2s$cv, cvs_h2s$h2))
-plot(cvs_h2s$cv, cvs_h2s$h2, xlab = 'CV', ylab = 'h2')
-
-
-# write results
-blues$Env <- as.factor(blues$Env)
-write.csv(blues[, c('Env', 'Hybrid', 'predicted.value')], 'output/blues.csv', row.names = F)
-
-cvs_h2s$Env <- as.factor(cvs_h2s$Env)
-write.csv(cvs_h2s, 'output/cvs_h2s.csv', row.names = F)
-
-
-# compare unadjusted means
-# ytrain <- rbind(read.csv('output/cv0/ytrain.csv'), read.csv('output/cv1/ytrain.csv'))
-# y <- merge(ytrain, blues, by = c('Env', 'Hybrid')) 
-# cor(y$Yield_Mg_ha, y$predicted.value)
-# cor(y$Yield_Mg_ha, y$predicted.value, method = 'spearman')
-# plot(y$Yield_Mg_ha, y$predicted.value)
 
