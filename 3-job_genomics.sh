4145c375cb92c1706d509346bf9d678ef573a704
will-the-g <wgatlin@chapman.edu>
2025-09-12 17:55:36 -0700

    Update 3-jobs_genomics.sh

--- 3-job_genomics.sh
+++ 3-job_genomics.sh
@@ -23,7 +23,7 @@ python3 -u src/create_individuals.py | tee "logs/individuals.txt"
 tail -n +2 output/individuals.csv | sed 's/^Hybrid//' > output/tmp.csv && mv output/tmp.csv output/individuals.csv
 
 ## filter VCF and create kinships matrices (you will need `vcftools` and `plink` here):
-vcftools --vcf "data/Training Data/5_Genotype_Data_All_2014_2025_Hybrids.vcf" --keep 'output/individuals.csv' --recode --recode-INFO-all --out output/maize_indiv
+vcftools --vcf "data/Training_Data/5_Genotype_Data_All_2014_2025_Hybrids.vcf" --keep 'output/individuals.csv' --recode --recode-INFO-all --out output/maize_indiv
 
 # filters variants by Minor Allele Frequency (MAF) so any SNP where the MAF is less than 0.01 is removed
 vcftools --vcf output/maize_indiv.recode.vcf --maf 0.01 --recode --recode-INFO-all --out output/maize_maf001
